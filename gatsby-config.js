debb0e89279330840e21567e5c773067b185d8f6
Matt <meinzer.matthew@gmail.com>
2017-10-12 19:52:47 -0500

    add Analytics tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-96186804-2`,
       },
     },
     `gatsby-plugin-offline`,
